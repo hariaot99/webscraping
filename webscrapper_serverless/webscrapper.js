69425e948e0ffa1f39703599f1da42a2464e20a6
Haria Othon <haria.othon@unesp.br>
2023-03-19 11:23:12 -0300

    Finalizado o webscrapper que busca os produtos mais vendidos da categoria cozinha

---
+++ webscrapper_serverless/webscrapper.js
@@ -0,0 +1,26 @@
+const chromium = require ('@sparticuz/chromium');
+const puppeteer = require ('puppeteer-core');
+
+module.exports.handle = async () => {
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  })
+
+  const page = await browser.newPage();
+  await page.goto('https://www.amazon.com.br/gp/bestsellers');
+
+  const cards = await page.evaluate(() =>
+  Array.from(document.querySelectorAll('#anonCarousel1 .a-carousel .p13n-sc-uncoverable-faceout'), (e) => ({
+    title: e.querySelector('.a-link-normal span').innerText,
+    url: e.querySelector('.a-link-normal').href,
+    price: e.querySelector('._cDEzb_p13n-sc-price_3mJ9Z').innerText,
+    imageUri: e.querySelector('.a-link-normal img').src,
+  })))
+  
+  await browser.close();
+  return(cards);
+};
