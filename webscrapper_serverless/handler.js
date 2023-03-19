aad81b480f849a8f41d9d7eac3a06240055532b3
Haria Othon <haria.othon@unesp.br>
2023-03-19 05:58:21 -0300

    Ajustando configurações e primeiros testes de webscraping

--- webscrapper_serverless/handler.js
+++ webscrapper_serverless/handler.js
@@ -1,24 +1,22 @@
-'use strict';
-const { getChrome } = require('./chrome-script');
-const puppeteer = require('puppeteer');
+const chromium = require ('@sparticuz/chromium');
+const puppeteer = require ('puppeteer-core');
 
- 
-module.exports.hello = async (event) => {
-  const { url } = event.queryStringParameters;
-  const chrome = await getChrome();
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: chrome.endpoint,
-  });
+module.exports.hello = async () => {
+
+  console.log("ENTROU NA FUNCAO");
+  const browser = await puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath(),
+    headless: chromium.headless,
+    ignoreHTTPSErrors: true,
+  })
+  console.log("PASSOU DA CRIACAO DO BROWSER");
   const page = await browser.newPage();
-  await page.goto(url, {waitUntil: 'networkidle0'});
-  const content = await page.evaluate(() => document.body.innerHTML);
-  return{
-    statusCode: 200,
-    body: JSON.stringify({
-      content,
-    }),
-  };
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+  console.log("Passou na criacao de pagina");
+  await page.goto('https://www.amazon.com.br/bestsellers');
+  console.log("ENTROU NA PAGINA INDICADA");
+  const title = await page.title();
+  await browser.close();
+  return(title);
+};
