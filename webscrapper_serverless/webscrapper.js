bc468f7d3a322b514eaca6900ef3b65037c2a66e
Haria Othon <haria.othon@unesp.br>
2023-03-19 13:09:46 -0300

    Configurando API Gateway e metodo get para a funcao

--- webscrapper_serverless/webscrapper.js
+++ webscrapper_serverless/webscrapper.js
@@ -1,26 +1,32 @@
 const chromium = require ('@sparticuz/chromium');
 const puppeteer = require ('puppeteer-core');
 
-module.exports.handle = async () => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    defaultViewport: chromium.defaultViewport,
-    executablePath: await chromium.executablePath(),
-    headless: chromium.headless,
-    ignoreHTTPSErrors: true,
-  })
-
-  const page = await browser.newPage();
-  await page.goto('https://www.amazon.com.br/gp/bestsellers');
-
-  const cards = await page.evaluate(() =>
-  Array.from(document.querySelectorAll('#anonCarousel1 .a-carousel .p13n-sc-uncoverable-faceout'), (e) => ({
-    title: e.querySelector('.a-link-normal span').innerText,
-    url: e.querySelector('.a-link-normal').href,
-    price: e.querySelector('._cDEzb_p13n-sc-price_3mJ9Z').innerText,
-    imageUri: e.querySelector('.a-link-normal img').src,
-  })))
+module.exports.handle = async (event, context) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath(),
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    })
   
-  await browser.close();
-  return(cards);
+    const page = await browser.newPage();
+    await page.goto('https://www.amazon.com.br/gp/bestsellers');
+  
+    const cards = await page.evaluate(() =>
+    Array.from(document.querySelectorAll('#anonCarousel1 .a-carousel .p13n-sc-uncoverable-faceout'), (e) => ({
+      title: e.querySelector('.a-link-normal span').innerText,
+      url: e.querySelector('.a-link-normal').href,
+      price: e.querySelector('._cDEzb_p13n-sc-price_3mJ9Z').innerText,
+      imageUri: e.querySelector('.a-link-normal img').src,
+    })))
+    
+    await browser.close();
+    response = {"statusCode":200, "body": JSON.stringify(cards)};
+    return(response);
+  } catch (error) {
+    return(error);
+  }
+  
 };
